fb7e6dcc218e610c7d19853ea87ef4ec5fe1f026
sonlh <sonbk1997>
2026-06-19 07:33:54 +0700

    Remove build trash

--- examples/lessons/09_character_driver/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/sonlh/3.LinuxEmbedded/embedded_linux_ai_first/examples/lessons/09_character_driver/modules.order := {   echo /home/sonlh/3.LinuxEmbedded/embedded_linux_ai_first/examples/lessons/09_character_driver/ioctl_led.o; :; } > /home/sonlh/3.LinuxEmbedded/embedded_linux_ai_first/examples/lessons/09_character_driver/modules.order
--- examples/lessons/09_character_driver/modules.order
+++
@@ -1,1 +0,0 @@
-/home/sonlh/3.LinuxEmbedded/embedded_linux_ai_first/examples/lessons/09_character_driver/ioctl_led.o
